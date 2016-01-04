2b2f51b0f9f372f9ce6ac70787acef9d07531050
slavavikharev <slavavikharev@142eb7da-3426-4e83-b16f-2be5c750212a>
2016-01-04 14:24:50 +0000

    Save/Load; Music; Pause

--- trunk/Pentix/button.py
+++ trunk/Pentix/button.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
 import pygame
 
+MOUSEDOWN = False
+
 
 def draw_button(surface,
                 msg, font_size, font_color,
                 x, y, width, height,
                 common_color, hover_color,
                 action=None, args=()):
+    global MOUSEDOWN
     mouse = pygame.mouse.get_pos()
     pygame.draw.rect(surface, (30, 30, 30), (x - 5, y + 5, width, height))
     if x + width > mouse[0] > x and y + height > mouse[1] > y:
         pygame.draw.rect(surface, hover_color, (x, y, width, height))
-        if pygame.mouse.get_pressed()[0] and action:
+        if pygame.mouse.get_pressed()[0] and action and not MOUSEDOWN:
             action(*args)
+        MOUSEDOWN = pygame.mouse.get_pressed()[0]
     else:
         pygame.draw.rect(surface, common_color, (x, y, width, height))
     font = pygame.font.SysFont("Arial", font_size)
--- trunk/Pentix/pentix.py
+++ trunk/Pentix/pentix.py
@@ -14,33 +14,16 @@ START = USEREVENT + 4
 
 
 def main():
-    game = gm.Game(False, (1024, 768))
+    resolution = (1024, 768)
+    game = gm.Game(False, resolution)
+    pygame.mixer.music.load('bg.mp3')
+    pygame.mixer.music.play(loops=-1)
     while not game.quit:
         game.draw()
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
-            elif event.type == KEYDOWN:
-                keys = pygame.key.get_pressed()
-                if keys[K_RIGHT] or keys[K_LEFT]:
-                    game.current.move_aside(game.level, keys)
-                    pygame.time.set_timer(MOVING, 300)
-                if keys[K_UP]:
-                    game.current.rotate(game.level)
-            elif event.type == KEYUP:
-                keys = pygame.key.get_pressed()
-                if not keys[K_LEFT] and not keys[K_RIGHT]:
-                    pygame.time.set_timer(MOVING, 0)
-            elif event.type == MOVING:
-                pygame.time.set_timer(MOVING, 60)
-                keys = pygame.key.get_pressed()
-                game.current.move_aside(game.level, keys)
-            elif event.type == FALLING:
-                game.current.fall_down(game)
-            elif event.type == BOOSTING:
-                keys = pygame.key.get_pressed()
-                game.current.move_down(game, keys)
             elif event.type == START:
                 temp = game.highscore
                 game = gm.Game(game.fullscreen, game.screen_size)
@@ -48,6 +31,28 @@ def main():
             elif event.type == VIDEORESIZE:
                 game.screen_size = event.dict['size']
                 game.draw_main()
+            if game.paused:
+                continue
+            if event.type == KEYDOWN:
+                keys = pygame.key.get_pressed()
+                if keys[K_RIGHT] or keys[K_LEFT]:
+                    game.current.move_aside(game, keys)
+                    pygame.time.set_timer(MOVING, 300)
+                if keys[K_UP]:
+                    game.current.rotate(game)
+            elif event.type == KEYUP:
+                keys = pygame.key.get_pressed()
+                if not keys[K_LEFT] and not keys[K_RIGHT]:
+                    pygame.time.set_timer(MOVING, 0)
+            elif event.type == MOVING:
+                pygame.time.set_timer(MOVING, 60)
+                keys = pygame.key.get_pressed()
+                game.current.move_aside(game, keys)
+            elif event.type == FALLING:
+                game.current.fall_down(game)
+            elif event.type == BOOSTING:
+                keys = pygame.key.get_pressed()
+                game.current.move_down(game, keys)
         pygame.display.update()
         game.fpsClock.tick(FPS)
     pygame.quit()
